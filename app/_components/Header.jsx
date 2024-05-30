e4fcfa4a1e9330bb50ef48499b3181319a92b99a
dragang1 <gajicdragan22@gmail.com>
2024-05-30 19:28:36 +0200

    Header && Auth

---
+++ app/_components/Header.jsx
@@ -0,0 +1,46 @@
+'use client';
+import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs';
+import { Plus } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+function Header() {
+    const path = usePathname();
+    const { user, isSignedIn } = useUser();
+
+
+    return (
+        <div className='p-6 px-10 flex justify-between items-center shadow-sm fixed top-0 w-full z-10 bg-white'>
+            <div className='flex gap-10 items-center'>
+                <div className='flex items-center gap-10'>
+
+                    <Image src={'/logo.svg'} width={100} height={100} alt='logo' />
+                    <h1 className='text-3xl text-[#fd7e14] '>Nekretnine</h1>
+                </div>
+                <ul className='hidden md:flex gap-10'>
+                    <Link href={'/'}>
+                        <li className={`hover:text-primary font-medium text-sm cursor-pointer ${path === '/' && 'text-primary'}`}>Prodaja</li>
+                    </Link>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer'>Iznajmljivanje</li>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer'>Agencije</li>
+                </ul>
+            </div>
+            <div className='flex gap-2'>
+                <Button className='flex gap-2' ><Plus className='h-5 w-5' /> Postavi svoj oglas</Button>
+                {isSignedIn ?
+                    <UserButton /> :
+                    <Link href={'/sign-in'}>
+
+                        <Button variant='outline' >Prijavi se</Button>
+                    </Link>
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+
+      <html lang="en">
+        <body className={inter.className}>
+
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
