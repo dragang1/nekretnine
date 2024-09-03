2dd148c68a4c509d26ce829c7ecb9899a8ca7365
dragang1 <gajicdragan22@gmail.com>
2024-09-03 09:30:12 +0200

    User settings

--- app/(routes)/user/_components/UserListing.jsx
+++ app/(routes)/user/_components/UserListing.jsx
@@ -1,8 +1,9 @@
 import { Button } from '@/components/ui/button';
 import { supabase } from '@/utils/supabase/client';
 import { useUser } from '@clerk/nextjs'
-import { Bath, BedDouble, MapPin, Ruler } from 'lucide-react';
+import { Bath, BedDouble, MapPin, Ruler, Trash } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner';
 
@@ -30,34 +31,40 @@ function UserListing() {
 
 
     return (
-        <div>
+        <div >
             <h2 className='font-bold text-xl'>Uredi oglase</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+            <div className='gap-4 flex flex-col '>
 
                 {listing && listing.map((item, index) => (
                     <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg' key={item.id} >
                         <h2 className='bg-primary text-white absolute px-2 text-sm p-1 m-1 rounded-lg'>{item.active ? 'Objavljen' : 'Sacuvan'}</h2>
-                        <Image src={item.listingImages[0]?.url}
+                        <Image src={item?.listingImages[0] ? item?.listingImages[0]?.url : '/placeholder.jpg'}
                             width={800}
                             height={150}
                             alt='propertyImage'
                             className='rounded-lg object-cover h-[170px]'
 
+
                         />
+
                         <div className='flex mt-2 flex-col gap-2'>
                             <h2 className='font-bold text-xl'>${item?.price}</h2>
-                            <h3 className='flex gap-2 text-sm text-gray-500'><MapPin className='w-4 h-4' />{item?.address}</h3>
+                            <h3 className='flex gap-2 text-sm text-gray-500 '><MapPin className='w-4 h-4' />{item?.address}</h3>
                         </div>
                         <div className='flex mt-2 gap-2  justify-between'>
                             <h2 className='flex gap-2 w-full text-sm bg-slate-300 rounded-md p-2 text-gray-500 justify-center items-center'><BedDouble className='h-4 w-4' />{item?.bedroom}</h2>
                             <h2 className='flex gap-2 w-full text-sm bg-slate-300 rounded-md p-2 text-gray-500 justify-center items-center'><Bath className='h-4 w-4' />{item?.bathroom}</h2>
                             <h2 className='flex gap-2 w-full text-sm bg-slate-300 rounded-md p-2 text-gray-500 justify-center items-center'><Ruler className='h-4 w-4' />{item?.area}</h2>
                         </div>
-                        <div className='flex  gap-2'>
+                        <div className='flex justify-between gap-2 mt-5'>
+                            <Link href={'/edit-listing/' + item.id} className='w-full'  >
+                                <Button size='sm' className='w-full' >Uredi</Button>
+                            </Link>
 
-                            <Button size='sm' className='w-full' type='outline' >Uredi</Button>
-                            <Button size='sm' className='w-full' >Pogledaj</Button>
-                            <Button size='sm' variant='destructive'>Obrisi</Button>
+                            <Link href={'/view-listing/' + item.id} className='w-full' >
+                                <Button size='sm' className='w-full'  >Pogledaj</Button>
+                            </Link>
+                            <Button size='sm' variant='destructive' className='w-full'><Trash /></Button>
                         </div>
                     </div>
                 ))}
